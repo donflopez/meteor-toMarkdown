d140eac4e90832f60360abed6a682eb5e262f297
Francisco López <donflopez@gmail.com>
2014-09-09 18:47:47 +0200

    Initial commit

---
+++ package.js
@@ -0,0 +1,18 @@
+Package.describe({
+  summary: "Conver html code into markdown",
+  version: "1.0.0",
+  git: ""
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('METEOR@0.9.1.1');
+  api.addFiles('donflopez:he.js');
+  api.addFiles('donflopez:tomarkdown.js');
+  api.export('toMarkdown', ['client', 'server']);
+});
+
+Package.onTest(function(api) {
+  api.use('tinytest');
+  api.use('donflopez:tomarkdown');
+  api.addFiles('donflopez:tomarkdown-tests.js');
+});
